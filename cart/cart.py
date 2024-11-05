179ab6b8169bc4d8c9dace89afefa0a08413cf5b
OmarMuhammmed <amorey2006@gmail.com>
2024-11-05 10:30:07 +0200

    add_gitignore_file

--- cart/cart.py
+++ cart/cart.py
@@ -1,4 +1,5 @@
 from store.models import Product,CustomerProfile
+
 class Cart:
     def __init__(self, request):
         self.session = request.session
@@ -9,51 +10,49 @@ class Cart:
         if not cart:
             cart = self.session['cart'] = {}
         self.cart = cart
+
     def save(self):
         ''' Mark the session as modified to save changes to the cart '''
         self.session.modified = True 
 
     def manage_cart(self) :
-      """ Each user has his own cart especially."""
-         # Deal with login user 
-      if self.request.user.is_authenticated:
-          current_user = CustomerProfile.objects.filter(user__id= self.request.user.id ) 
-          # Convert {'3':1, '2':4} to {"3":1, "2":4} B Json not error
-          carty = str(self.cart)
-          carty = carty.replace("\'", "\"") # \ To ignore error
-          # Save carty to the CustomerProfile Model
-          current_user.update(old_cart=str(carty))
+        """ Each user has his own cart especially."""
+        # Deal with login user 
+        if self.request.user.is_authenticated:
+            current_user = CustomerProfile.objects.filter(user__id= self.request.user.id ) 
+            # Convert {'3':1, '2':4} to {"3":1, "2":4} B Json not error
+            carty = str(self.cart)
+            carty = carty.replace("\'", "\"") # \ To ignore error
+            # Save carty to the CustomerProfile Model
+            current_user.update(old_cart=str(carty))
       
 
     def db_add(self, product, quantity):
-      product_id = str(product) 
-      product_qty = int(quantity)
-      
-      if product_id in self.cart:
-          self.cart[product_id] += product_qty  
-      else: 
-          self.cart[product_id] = product_qty  
-      
-      self.save()
-      self.manage_cart()
+        product_id = str(product) 
+        product_qty = int(quantity)
+        
+        if product_id in self.cart:
+            self.cart[product_id] += product_qty  
+        else: 
+            self.cart[product_id] = product_qty  
+        
+        self.save()
+        self.manage_cart()
      
-
-
-
-
+     
     def add(self, product, quantity):
-      # Convert product ID to string to use as a key in the cart dictionary
-      product_id = str(product.id) 
-      # Convert the quantity to an integer
-      product_qty = int(quantity)
-      
-      if product_id in self.cart:
-          self.cart[product_id] += product_qty  
-      else: 
-          self.cart[product_id] = product_qty  
-      
-      self.save()
-      self.manage_cart()
+        # Convert product ID to string to use as a key in the cart dictionary
+        product_id = str(product.id) 
+        # Convert the quantity to an integer
+        product_qty = int(quantity)
+        
+        if product_id in self.cart:
+            self.cart[product_id] += product_qty  
+        else: 
+            self.cart[product_id] = product_qty  
+        
+        self.save()
+        self.manage_cart()
      
 
     def __len__(self):
